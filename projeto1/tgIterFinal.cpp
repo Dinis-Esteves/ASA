3d8956126b4ece949e5a6b170edd6e36ab25f826
Dinis Esteves <dinis.esteves@tecnico.ulisboa.pt>
2024-12-02 20:51:50 +0000

    comment and debug remove & creation of the foldert relative to the report

--- projeto1/tgIterFinal.cpp
+++ projeto1/tgIterFinal.cpp
@@ -5,49 +5,14 @@
 #include <vector> 
 
 using namespace std; 
-
 // O(m²/2 * n²) probably mine is like this
-
 vector<vector<int>> table; 
 
 vector<int> sequence;
 
 vector<vector<vector<pair<tuple<int, int, int>, tuple<int, int, int>>>>> pl;
 
-// Function to print a tuple of three integers
-void printTuple(const std::tuple<int, int, int>& t) {
-    std::cout << "(" 
-              << std::get<0>(t) << ", " 
-              << std::get<1>(t) << ", " 
-              << std::get<2>(t) << ")";
-}
-
-// Function to print a pair of tuples
-void printPairOfTuples(const std::pair<std::tuple<int, int, int>, std::tuple<int, int, int>>& p) {
-    std::cout << "First tuple: ";
-    printTuple(p.first);
-    std::cout << "\nSecond tuple: ";
-    printTuple(p.second);
-    std::cout << std::endl;
-}
-
-void printdp(vector<vector<vector<int>>> dp, int seqLen, int maxInt) {
-
-    for(int i = 0; i < seqLen; i++) {
-        for(int j = 0; j < seqLen; j++) {
-            printf("{");
-            for(int k = 0; k < maxInt; k++) {
-                printf("%d ", dp[i][j][k]);
-            }
-            printf("}");
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-
 string generateExp(tuple<int, int, int> p) {
-
     pair<tuple<int, int, int>, tuple<int, int, int>> c; 
 
     if (get<0>(p) == get<1>(p)) {
@@ -61,7 +26,6 @@ string generateExp(tuple<int, int, int> p) {
 }
 
 string findExpression(vector<int> sequence, int target, int seqLen, int maxInt) {
-
     // handle the caise when seqlen is 1
     if (seqLen == 1 && target == sequence[0]) {
         return to_string(sequence[0]);
@@ -76,7 +40,6 @@ string findExpression(vector<int> sequence, int target, int seqLen, int maxInt) 
             dp[i][i][0] = sequence[i];
     }
 
-    //printdp(dp, seqLen, maxInt);
     for (int k = 0; k < seqLen - 1; k++) {
         for (int i = 0; i < seqLen - 1; i++) {
         
@@ -102,17 +65,9 @@ string findExpression(vector<int> sequence, int target, int seqLen, int maxInt) 
 
                         if (right < 0 || left < 0) {break;}
 
-                        //printf("i: %d, u+1: %d, l: %d\n", i, u{1}, l);
-                        //printf("u: %d, j: %d, m: %d\n", u, j, m);
-                        //cout << endl;
-                        //printf("r: %d, l: %d\n", right, left);
-
                         if (left >= 0 && right >= 0) {
                           int result = table[left][right];
-                          //printf("Processing: i=%d, j=%d, n=%d, result=%d\n", i, j, n, result);
-                          // printf("result: %d\n", result);
-                          // printf("bool: %d\n", stored_results.find(result)
-                          // == stored_results.end()); case when we are
+                          // case when we are
                           // calculating the final options and find one that
                           // fits in the result
                           if (k == seqLen - 2 && result == target) {
@@ -129,23 +84,14 @@ string findExpression(vector<int> sequence, int target, int seqLen, int maxInt) 
                             dp[i][j][0] = target;
                             pl[i][j][0] = p;
 
-                              //pair<tuple<int,int, int>, tuple<int, int, int>> c = pl[i][j][n];
-                              //printf("(%d, %d, %d)\n", i, j, n);
-                              //printPairOfTuples(c);
-                              //printf("\n");
-
-                            //printdp(dp, seqLen, maxInt);
                             tuple<int, int, int> p2;
                             get<0>(p2) = 0;
                             get<1>(p2) = seqLen - 1;
                             get<2>(p2) = 0;
-                            //printf("Target reached: %d at dp[%d][%d][%d]\n", result, i, j, n);
-                            //printdp(dp, seqLen, maxInt);
 
                             return generateExp(p2);
                             } else if (k != seqLen - 2 && stored_results.find(result) == stored_results.end()) {
                               dp[i][j][n] = result;
-                              // printdp(dp, seqLen, maxInt);
 
                               pair<tuple<int, int, int>, tuple<int, int, int>> p;
 
@@ -159,26 +105,7 @@ string findExpression(vector<int> sequence, int target, int seqLen, int maxInt) 
 
                               pl[i][j][n] = p;
 
-                              //pair<tuple<int,int, int>, tuple<int, int, int>> c = pl[i][j][n];
-
-                              //printf("(%d, %d, %d)\n", i, j, n);
-                              //printPairOfTuples(c);
-                              //printf("\n");
-                              
-                              /*
-                              tuple<int, int, int> d;
-                              get<0>(d) = i;
-                              get<1>(d) = j;
-                              get<2>(d) = n;
-
-
-                              printPairOfTuples(p);
-
-                              printf("Stored result: %d at dp[%d][%d][%d]\n", result, i, j, n);
-                              printf("%s\n\n", generateExp(d).c_str());
-*/
                               stored_results.insert(result);
-                              
                               n++;
                             }
                         } 
@@ -186,7 +113,6 @@ string findExpression(vector<int> sequence, int target, int seqLen, int maxInt) 
                 }
                 u--;
             }
-
             if (j == seqLen - 1) {
                 break;
                 
